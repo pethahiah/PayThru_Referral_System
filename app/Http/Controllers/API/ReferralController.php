d41f61a57853a56b9aa9271d036fe5f0dc07d720
lumsys <sunday4oged@yahoo.com>
2022-03-16 17:00:06 +0100

    get Referred

--- app/Http/Controllers/API/ReferralController.php
+++ app/Http/Controllers/API/ReferralController.php
@@ -10,18 +10,25 @@ use App\User;
 class ReferralController extends Controller
 {
     //
-public function getReferred(){
+// public function getReferred(){
 
-    $aff = User::where(['usertype' => 'developer'])
-    ->select('name', 'email', 'phone', 'created_at')->get();
-    return response()->json(['message' => 'success', 'data'=>$aff], 200);   
+//     $aff = User::where(['usertype' => 'developer'])
+//     ->select('name', 'email', 'phone', 'created_at')->get();
+//     return response()->json(['message' => 'success', 'data'=>$aff], 200);   
 
-    }
+//     }
 
 
     public function getAffiliateId(){
         $id = Auth::user();
         $getAffiliate = user::where('id', $id->id)->where('usertype', 'developer')->select('affiliate_id')->get();
+        return response()->json($getAffiliate);
+
+    }
+
+    public function getReferred(){
+        $id = Auth::user();
+        $getAffiliate = user::where('id', $id->id)->where('usertype', 'developer')->where('referred_by', $id->affiliate_id)->select('name', 'email', 'phone', 'created_at')->get();
         return response()->json($getAffiliate);
 
     }
